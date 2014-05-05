2d72c180833615da551e362b197ff3797e04d04a
gorbenko-vladislav <gorbenko.vladislav@gmail.com>
2014-05-05 09:19:54 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BdSchool::Application.config.secret_key_base = 'e8f89de077f668c6423f1b29667b17295e242bb21dcc1442db1bd09cdc7d94b7c821416724cc81574a4f2fc70b61ee2d0eb84c5e764236e27ba03792dc02eec7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BdSchool::Application.config.session_store :cookie_store, key: '_bd_school_session'
