e7ccd4df3a7aa4cbcb7f572a51f073589efd2254
gorbenko-vladislav <gorbenko.vladislav@gmail.com>
2014-06-22 18:43:48 +0300

    Added column Birthday to User mode

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621155953) do
+ActiveRecord::Schema.define(version: 20140622125704) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140621155953) do
     t.datetime "last_invited_action"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "birthday"
   end
 
 end
